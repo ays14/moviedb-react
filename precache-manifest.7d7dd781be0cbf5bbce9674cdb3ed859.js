d508951f72bf81f929c56ddcf834f5abdd6b3990
ays14 <ayush.sh143@gmail.com>
2019-05-31 13:59:50 +0530

    Updates

---
+++ precache-manifest.7d7dd781be0cbf5bbce9674cdb3ed859.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16934b82769b973d31e06cfea2f61c0c",
+    "url": "/moviedb-react/index.html"
+  },
+  {
+    "revision": "e8c73f66ce43a79dcf2e",
+    "url": "/moviedb-react/static/js/2.7d2d8e13.chunk.js"
+  },
+  {
+    "revision": "2aaaf35ee72cc67567b8",
+    "url": "/moviedb-react/static/js/main.79111a91.chunk.js"
+  },
+  {
+    "revision": "3097c18ce8440f1fdca5",
+    "url": "/moviedb-react/static/js/runtime~main.db341e92.js"
+  }
+]);
