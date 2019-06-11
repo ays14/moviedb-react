d622a1a9ca361f2f615dceaf9f14103220f9fcee
ays14 <ayush.sh143@gmail.com>
2019-06-11 15:03:15 +0530

    version 1.6 - minor changes

--- src/components/SearchBar/SearchResults.js
+++ src/components/SearchBar/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import MovieCard from '../MovieCard';
+import MovieCard from './MovieCard';
 
 const Content = styled.div`
 	text-align: center;
